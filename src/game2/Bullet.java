e13408c44a16936ff38e42ccc6b3c9dabe37baf5
Amanda Ma <amma@vassar.edu>
2014-11-14 10:25:21 -0500

    Jay is magic
    Test is done for checking outOfBound

--- src/game2/Bullet.java
+++ src/game2/Bullet.java
@@ -5,11 +5,7 @@
  */
 
 package game2;
-import java.awt.Color;
 import java.util.Random;
-import javalib.funworld.*;
-import javalib.colors.*;
-import javalib.worldcanvas.*;
 import javalib.worldimages.*;
 
 /**
@@ -33,7 +29,7 @@ public class Bullet {
     //1, going down, -1 going up
     int direction;
     
-    Bullet(Posn position, int color, int screenWidth, int ScreenHeight, int direction){
+    Bullet(Posn position, int color, int screenWidth, int screenHeight, int direction){
         this.position = position;
         this.screenWidth = screenWidth;
         this.screenHeight = screenHeight;
@@ -48,16 +44,16 @@ public class Bullet {
                             this.screenHeight, this.direction);
     }
     
+    //true for out of bounds
     public boolean outOfBounds(){
-        return (this.position.y < this.bulletRadius/2) || 
-                (this.position.y > this.screenHeight - this.bulletRadius/2);
+        return (this.position.y < 0 || this.position.y > this.screenHeight);
     }
     
     
     //testing 
     
     static int checkOnTick;
-    
+    static int checkOutOfBounds;
     
     static Random rand = new Random();
     
@@ -77,7 +73,7 @@ public class Bullet {
                 randInt = 1;
             }
             
-            Posn randPosn = new Posn(randInt(15, 285), randInt(50, 600));
+            Posn randPosn = new Posn(randInt(0, 300), randInt(0, 600));
             
             Bullet bullet1 = new Bullet(randPosn, randInt(1,3), 300, 600, randInt);
             Bullet bullet2 = bullet1.onTick();
@@ -95,10 +91,65 @@ public class Bullet {
         }
     }
     
+    private static void checkOutOfBounds() throws Exception {
+        for (int i = 0; i < 1000; i++) {
+            
+            //decides the trajectory of the bullet
+            int randInt = randInt(0, 2);
+            if (randInt == 0) {
+                randInt = -1 ;
+            } else {
+                randInt = 1;
+            }
+            
+            //random Positions
+            int randX = randInt(0,300);
+            int randY = randInt(0, 600);
+            Posn randPosn = new Posn(randX, randY);
+            
+            //screen dimmensions
+            int screenWidth = 300;
+            int screenHeight = 600;
+            
+            //creating the bullets to compare
+            Bullet bullet1 = new Bullet(randPosn, randInt(1,3), screenWidth, screenHeight, randInt);
+            Bullet bullet2 = bullet1; 
+            
+            int randTickInt = randInt(0, 50);
+            
+            for(int k = 0; k < randTickInt; k++){
+                bullet2 = bullet2.onTick();
+            }
+            
+            //what is potentially the bullet placement after the ticking
+            int bulletAfterPosn = randY + (randInt * (randTickInt * bullet1.moveRate));
+            
+            
+            //checks the cases of being bound
+            if(randInt == 1 && bulletAfterPosn > screenHeight){
+                if (!bullet2.outOfBounds()) {
+                   throw new Exception("The bullet is actually out of bounds on the bottom");
+                }
+            } else if (randInt == -1 && bulletAfterPosn < 0) { 
+                if (!bullet2.outOfBounds()){
+                    throw new Exception("The bullet is actually out of bounds on the top");
+                }
+            } else {
+                if (bullet2.outOfBounds()){
+                    throw new Exception("The bullet is actually within the bounds of the screen: ");
+                }
+            }
+            
+            checkOutOfBounds++;
+        }
+    }
+    
     public static void main(String[] args) throws Exception {
         checkOnTick();
+        checkOutOfBounds();
         
         System.out.println("checkOnTick passed " + checkOnTick + " times");
+        System.out.println("checkOutOfBounds passed " + checkOutOfBounds + "times");
     }
     
     
